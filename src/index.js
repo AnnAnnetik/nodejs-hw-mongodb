8a04a1ba06f37e6cffa04c1790089703367dc36d
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-05-21 13:09:23 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+
+bootstrap();
+
+// mongodb+srv://eWP8Vn4WbnqaxHoC:<password>@cluster0.c87ywqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+/*<password>  eWP8Vn4WbnqaxHoC */
