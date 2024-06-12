d9074fe28422f1a6c328618b59938989e1657fff
AnnAnnetik <143535720+AnnAnnetik@users.noreply.github.com>
2024-06-12 13:53:31 +0300

    1

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ export const setupServer = () => {
   );
 
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     express.json({
@@ -33,7 +34,6 @@ export const setupServer = () => {
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
-  app.use(cookieParser());
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
